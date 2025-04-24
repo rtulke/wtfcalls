90725bc156dc0118a98879db5132e75aef714d46
Robert Tulke <rt@debian.sh>
2025-04-24 13:22:16 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+WTFCalls - Live detector for outgoing network calls.
+"""
+
+__version__ = "0.2.0"
+__author__ = "Robert Tulke"
+
+# Import main functions to make them available at package level
+from wtfcalls import main, parse_arguments
